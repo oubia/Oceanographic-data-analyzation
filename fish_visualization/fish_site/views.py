cfd493379c590a915b10e70ea43ac2c54c89e92a
oubia <mohamed4oubia@gmail.com>
2021-02-22 23:34:16 +0100

    django templates and configuration

--- fish_visualization/fish_site/views.py
+++ fish_visualization/fish_site/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.urls import path
 from django.http import HttpResponse 
-
+import pandas
 
 
 
 # Create your views here.
 
 def home(request):
-    return (request,'home/dashboard.html')
+    # df = pandas.read_csv('csv.csv')
+    # print(df)
+    return render(request, 'home/dashboard.html')
+
+def TablePage(request):
+    
+    return render(request, 'home/table.html')
