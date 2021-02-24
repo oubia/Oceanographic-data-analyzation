702228340d3acf8738ce3001d87e4c70bd92bd04
oubia <mohamed4oubia@gmail.com>
2021-02-24 23:26:41 +0100

    table dynamic

--- fish_visualization/fish_site/views.py
+++ fish_visualization/fish_site/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from django.urls import path
 from django.http import HttpResponse 
-import pandas
+import pandas as pd
 
 
 
 # Create your views here.
 
 def home(request):
-    # df = pandas.read_csv('csv.csv')
-    # print(df)
+    
     return render(request, 'home/dashboard.html')
 
+
+
+def DataName(df):
+    return df.columns
 def TablePage(request):
-    
-    return render(request, 'home/table.html')
+    df = pd.read_csv('E:/homy/S4/PFE/PFE/fish_visualization/fish_site/Data/Book1.csv', encoding = "cp1252")
+    # a= df.set_index('Species (common name)')
+    # b=a[a.isin(['Cephalopods']).any(axis=1)]
+    b=DataName(df)
+    c=len(b)
+    context={'b':b,'c':c}
+    return render(request, 'home/table.html',context)
