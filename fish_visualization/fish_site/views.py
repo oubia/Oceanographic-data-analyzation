cfa496b83c1b4045a65f5e840789e22c85a7f66f
oubia <mohamed4oubia@gmail.com>
2021-03-17 20:23:26 +0100

    before integration

--- fish_visualization/fish_site/views.py
+++ fish_visualization/fish_site/views.py
@@ -7,11 +7,6 @@ import pandas as pd
 
 # Create your views here.
 
-def home(request):
-    
-    return render(request, 'home/dashboard.html')
-
-
 # change all dataframe to key value
 def ChangeToDic(df):
     l=df.columns
@@ -19,35 +14,32 @@ def ChangeToDic(df):
     value_list=l
     g=dict(zip(key_list,value_list))
     return g
-# def DataName(df):
-    # return df.columns
 
-# change all dataframe to key value
-def ChangeToDic(df):
-    key_list=df
-    value_list=df
-    g=dict(zip(key_list,value_list))
-    return g
-def search(GroupType,SpecieName):
-    df = pd.read_csv('E:/homy/S4/PFE/PFE/fish_visualization/fish_site/Data/Book1.csv', encoding = "cp1252")
-    dataDict=df.to_dict('index')
-    data=[]
-    for i in range (len(dataDict)):
-        if dataDict[i][GroupType] == SpecieName:
-            data.append(dataDict[i])
-    return data     
-# def Convert_dict(List):  
-#     init = iter(List)  
-#     res_dct = dict(zip(init, init))  
-#     return res_dct
+def home(request):
+    return render(request, 'home/dashboard.html')
 
 
 def TablePage(request):
     df = pd.read_csv('E:/homy/S4/PFE/PFE/fish_visualization/fish_site/Data/Book1.csv', encoding = "cp1252")
     columnsName=ChangeToDic(df)
-    GroupType = 'Functional group'
-    SpecieName = 'Cephalopods'
-    Data=search(GroupType,SpecieName)
-    print(Data) 
-    context={'columnsName':columnsName,'Data':Data}
+    columnsName = columnsName.keys()
+    GlobalData = []
+    for i in range(df.shape[0]):
+        temp = df.loc[i]
+        GlobalData.append(dict(temp))
+    l = []
+    for i in GlobalData:
+        l.append(i.values())
+    L=[]
+    spieces = (df['Functional group'])
+    for i in l:
+        if 'Sharks and rays' in i:
+            L.append(i)
+    if request.method == 'POST':
+        print("psot is -----------",request.method)
+    else:
+        print(request)
+
+    context={'columnsName':columnsName ,'L':L,'spieces':spieces}
     return render(request, 'home/table.html',context)
+
